ae8e1328bb20b73b9e1aa921771cb7682e6dff48
kh618 <khaled.abdelnabi.18@gmail.com>
2020-08-24 02:49:47 +0200

    - add MessageInfoModel.kt

---
+++ app/src/main/java/com/abdelnabi/messagevisualizer/model/MessageInfoModel.kt
@@ -0,0 +1,10 @@
+package com.abdelnabi.messagevisualizer.model
+
+import com.google.android.gms.maps.model.LatLng
+
+class MessageInfoModel {
+    var id: String? = null
+    var message: String? = null
+    var sentiment: String? = null
+    var latLng: LatLng? = null
+}

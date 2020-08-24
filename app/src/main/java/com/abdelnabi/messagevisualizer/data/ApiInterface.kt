17c20fdc7b8638b5c5ab46b8156c0cd97a94849e
kh618 <khaled.abdelnabi.18@gmail.com>
2020-08-24 02:35:24 +0200

    - add get message function to interface

--- app/src/main/java/com/abdelnabi/messagevisualizer/data/ApiInterface.kt
+++ app/src/main/java/com/abdelnabi/messagevisualizer/data/ApiInterface.kt
@@ -1,7 +1,13 @@
 package com.abdelnabi.messagevisualizer.data
 
+import retrofit2.Call
+import com.abdelnabi.messagevisualizer.model.MessageModel
+import retrofit2.http.GET
+
 
 interface ApiInterface {
 
+    @GET("feeds/list/0Ai2EnLApq68edEVRNU0xdW9QX1BqQXhHRl9sWDNfQXc/od6/public/basic?alt=json")
+    fun getMessage(): Call<MessageModel>
 
 }

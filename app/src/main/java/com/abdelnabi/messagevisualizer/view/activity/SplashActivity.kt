29149d941b9d64b3e7fdd2dc272ffb2805eb7483
kh618 <khaled.abdelnabi.18@gmail.com>
2020-08-24 02:00:23 +0200

    - open main activity after splash

--- app/src/main/java/com/abdelnabi/messagevisualizer/view/activity/SplashActivity.kt
+++ app/src/main/java/com/abdelnabi/messagevisualizer/view/activity/SplashActivity.kt
@@ -11,7 +11,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler().postDelayed({
-            ini
+            val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)

8774226dd97fd01d1be2f0ba0eff758c30ed0331
kh618 <khaled.abdelnabi.18@gmail.com>
2020-08-24 03:37:08 +0200

    - add message view model

---
+++ app/src/main/java/com/abdelnabi/messagevisualizer/view_model/MessageViewModel.kt
@@ -0,0 +1,61 @@
+package com.abdelnabi.messagevisualizer.view_model;
+
+import android.util.Log
+import androidx.constraintlayout.widget.Constraints
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.abdelnabi.messagevisualizer.data.ApiClient
+import com.abdelnabi.messagevisualizer.model.MessageInfoModel
+import com.abdelnabi.messagevisualizer.model.MessageModel
+import com.abdelnabi.messagevisualizer.model.getMessageInfoFromString
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MessageViewModel : ViewModel() {
+
+    val mutableLiveData =
+        MutableLiveData<List<MessageInfoModel>>()
+    val onErrorThrowableMutableLiveData =
+        MutableLiveData<String>()
+
+    fun getMessage() {
+        val itemCall = ApiClient.instance!!.getMessage()
+        itemCall.enqueue(object : Callback<MessageModel> {
+            override fun onResponse(
+                call: Call<MessageModel>,
+                response: Response<MessageModel>
+            ) {
+                if(response.code()/100 == 2){
+                    val body = response.body()!!
+                    mutableLiveData.value = getMessageInfo(body.feed!!.entry!!)
+                }else{
+                    onErrorThrowableMutableLiveData.postValue("We faced a problem, please try in another time")
+                }
+
+            }
+
+            override fun onFailure(
+                call: Call<MessageModel>,
+                t: Throwable
+            ) {
+                Log.e(
+                    Constraints.TAG,
+                    "onFailure: ",
+                    t
+                )
+                onErrorThrowableMutableLiveData.value = "We faced a problem, please try in another time"
+            }
+        })
+    }
+
+    fun getMessageInfo(dataList : List<MessageModel.EntryEntity>): List<MessageInfoModel>{
+        val messageList = mutableListOf<MessageInfoModel>()
+        for(item in dataList){
+            messageList.add(getMessageInfoFromString(item.content!!.messageBody!!)!!)
+        }
+        return messageList
+    }
+
+
+}
